352b55af51c27071aee60484bfad02064253be25
LightMS <54469865+LightMS@users.noreply.github.com>
2021-11-10 18:06:10 +0300

    Add files via upload

---
+++ Labwork4/Labwork4.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labwork4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            List<int> FirstMnozh = new List<int>() { 1, 4, 3, 2, 5, 7 };
+            List<int> SecondMnozh = new List<int>() { 6, 10, 25, 1, 4, 121 };
+            List<int> FinalMnozh = new List<int>();
+            foreach (int i in FirstMnozh)
+            {
+                if (SecondMnozh.Contains(i))
+                {
+                    FinalMnozh.Add(i);
+                }
+            }
+            foreach (int i in FinalMnozh)
+            {
+                Console.WriteLine(i);
+            }
+            //Задание 2
+            Queue<int> FirstQueve = new Queue<int>();
+            FirstQueve.Enqueue(1);
+            FirstQueve.Enqueue(2);
+            FirstQueve.Enqueue(0);
+            FirstQueve.Enqueue(3);
+            FirstQueve.Enqueue(5);
+            int min = FirstQueve.Peek();
+            int max = FirstQueve.Peek();
+            int minind = 0;
+            int maxind = 0;
+            foreach (int i in FirstQueve)
+            {
+                if (i < min)
+                {
+                    min = i;
+                    minind = Array.IndexOf(FirstQueve.ToArray(), i);
+                }
+                else if (i > max)
+                {
+                    max = i;
+                    maxind = Array.IndexOf(FirstQueve.ToArray(), i);
+                }
+            }
+            int sum = 0;
+            int[] a = FirstQueve.ToArray();
+            for (int i = minind; i <= maxind; i++)
+            {
+                sum += a[i];
+            }
+            Console.WriteLine(minind);
+            Console.WriteLine(maxind);
+            Console.WriteLine(sum);
+            // Задание 3
+            List<int> HumanNumber = new List<int>();
+            int n = 10;
+            for (int i = 1; i <= n; i++)
+            {
+                HumanNumber.Add(i);
+            }
+            do
+            {
+                for (int i = 1; i < HumanNumber.Count; i += 2)
+                {
+                    HumanNumber.RemoveAt(i);
+                }
+            } while (HumanNumber.Count != 1);
+
+            for (int i = 0; i < HumanNumber.Count; i++)
+            {
+                Console.WriteLine(HumanNumber[i]);
+            }
+        }
+    }
+}
---
+++ Labwork4/Labwork42.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Labwork42
+{
+    class Program
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            var t = a;
+            a = b;
+            b = t;
+        }
+        static int[] ShellSort(int[] array)
+        {
+            //расстояние между элементами, которые сравниваются
+            var d = array.Length / 2;
+            while (d >= 1)
+            {
+                for (var i = d; i < array.Length; i++)
+                {
+                    var j = i;
+                    while ((j >= d) && (array[j - d] > array[j]))
+                    {
+                        Swap(ref array[j], ref array[j - d]);
+                        j = j - d;
+                    }
+                }
+
+                d = d / 2;
+            }
+
+            return array;
+        }
+
+        static int[] BubbleSort(int[] array)
+        {
+            var len = array.Length;
+            for (var i = 1; i < len; i++)
+            {
+                for (var j = 0; j < len - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите элементы массива: ");
+            var parts = Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+            var array = new int[parts.Length];
+            for (int i = 0; i < parts.Length; i++)
+            {
+                array[i] = Convert.ToInt32(parts[i]);
+            }
+            Console.WriteLine("Каким способом вы хотите сделать сортировку? (Введите 1 для сортировки пузырьком и 2 для сортировки Шелла)");
+            string Choice = Console.ReadLine();
+            while(true)
+            {
+                Console.WriteLine("Ошибка. Попробуйте ещё раз");
+                if (Choice == "1")
+                {
+                    Console.WriteLine("Отсортированный массив: {0}", string.Join(", ", BubbleSort(array)));
+                    break;
+                }
+                else if (Choice == "2")
+                {
+                    Console.WriteLine("Отсортированный массив: {0}", string.Join(", ", ShellSort(array)));
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка. Попробуйте ещё раз (Введите 1 для сортировки пузырьком и 2 для сортировки Шелла)");
+
+                }
+            }
+        }
+    }
+}
